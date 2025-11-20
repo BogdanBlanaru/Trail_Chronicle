afbd9d744f56de78ad128b56663826b656e9b004
Bogdan blanaru <bogdansebastian200@yahoo.com>
2025-11-20 23:47:16 +0200

    fix the missing photo

--- lib/trail_chronicle_web/endpoint.ex
+++ lib/trail_chronicle_web/endpoint.ex
@@ -15,6 +15,11 @@ defmodule TrailChronicleWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+  plug Plug.Static,
+    at: "/uploads",
+    from: "priv/static/uploads",
+    gzip: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
